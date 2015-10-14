087aef4841de04ad93cde049d633b4d650449e3f
Christoph Frick <rid@ofnir.net>
2015-10-14 18:30:54 +0200

    Step 5: a mutate function

--- src/try_om_next/core.cljs
+++ src/try_om_next/core.cljs
@@ -16,11 +16,27 @@
       {:value v}
       {:value :not-found})))
 
-(def my-parser (om/parser {:read read}))
+(defn mutate
+  [{:keys [state] :as env} key params]
+  (do ; additional debug
+    (println "Mutate called with:")
+    (println @state)
+    (println key)
+    (println params))
+  (if (= 'increment key)
+    {:value [:counter]
+     :action #(swap! state update-in [:counter] inc)}
+    {:value :not-found}))
+
+(def my-parser (om/parser {:read read :mutate mutate}))
 
 (def my-state (atom {:counter 0}))
 
 (println (my-parser {:state my-state} [:counter :title]))
+
+(my-parser {:state my-state} '[(increment)])
+
+(println @my-state)
 
 (defn on-js-reload []
   ;; optionally touch your app-state to force rerendering depending on
