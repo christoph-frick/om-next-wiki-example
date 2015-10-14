15e65b3e91db5f2dd76719f840c5c1d1c10ea0e0
Christoph Frick <rid@ofnir.net>
2015-10-14 17:59:02 +0200

    Step 2.1: hello world with props added multiple times

--- src/try_om_next/core.cljs
+++ src/try_om_next/core.cljs
@@ -12,8 +12,17 @@
 (def app (om/factory HelloWorld))
 
 (js/React.render 
-  (app {:title "Hello Props"}) 
+  (apply dom/p nil
+         (map #(app {:title (str "Hello " %)})
+              (range 3)))
   (js/document.getElementById "app"))
+
+;;; fails to reload (e.g. change the number in the range)
+; Uncaught Error: Invariant Violation: processUpdates(): Unable to find child
+; 0 of element. This probably means the DOM was unexpectedly mutated (e.g., by
+; the browser), usually due to forgetting a <tbody> when using tables, nesting
+; tags like <form>, <p>, or <a>, or using non-SVG elements in an <svg> parent.
+; Try inspecting the child nodes of the element with React ID `.0`.
 
 (defn on-js-reload []
   ;; optionally touch your app-state to force rerendering depending on
